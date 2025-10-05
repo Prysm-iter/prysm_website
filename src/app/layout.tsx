b678ff033b58be4944a95e391b4d21399c91fb6e
Aryan Agrawal <153417567+aryanagr-9114@users.noreply.github.com>
2025-10-05 23:45:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 pt-24">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
